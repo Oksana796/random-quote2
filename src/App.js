099952571ae209ab2265a59e3f5f366a6634ede5
Oksana <ksenia79460@gmail.com>
2023-02-07 17:37:33 +0000

    DOM, import icons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
+import { FaTwitter, FaQuoteLeft } from "react-icons/fa";
+
 import "./App.css";
 
 function App() {
   return (
     <>
       <div className="App">
-        <div className="text"></div>
+        <div className="text">
+          <div className="quote">
+            <FaQuoteLeft />
+          </div>
+          <div className="author">-</div>
+        </div>
+
         <div className="buttons">
           <a
-            class="twitter-share-button"
+            className="button"
             target="_blank"
             rel="noreferrer"
             href="https://twitter.com/intent/tweet?text=Hello%20world&hashtags=proudOf via @me"
-            data-size="large"
+            title="Tweet this quote!"
           >
-            Tweet
+            <FaTwitter />
           </a>
+          <button className="button">New Quote</button>
         </div>
       </div>
-      <footer></footer>
+      <footer>
+        by 
+        <a href="https://github.com/Oksana796" target="_blank" rel="noreferrer">
+           Oksana
+        </a>
+      </footer>
     </>
   );
 }
