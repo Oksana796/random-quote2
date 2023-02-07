298a6558bc707b3de1ef76d7a89aa81525d9eb3a
Oksana <ksenia79460@gmail.com>
2023-02-07 16:55:55 +0000

    install icons, fonts, tweet button

--- src/App.js
+++ src/App.js
@@ -5,7 +5,17 @@ function App() {
     <>
       <div className="App">
         <div className="text"></div>
-        <div className="buttons"></div>
+        <div className="buttons">
+          <a
+            class="twitter-share-button"
+            target="_blank"
+            rel="noreferrer"
+            href="https://twitter.com/intent/tweet?text=Hello%20world&hashtags=proudOf via @me"
+            data-size="large"
+          >
+            Tweet
+          </a>
+        </div>
       </div>
       <footer></footer>
     </>
