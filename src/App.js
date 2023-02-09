d14cd5cdaa370ce4815522da4a481a4a4afa644b
Oksana <ksenia79460@gmail.com>
2023-02-09 15:14:32 +0000

    fetch data with useState

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
+import React, { useState, useEffect } from "react";
 import { FaTwitter, FaQuoteLeft } from "react-icons/fa";
 
 import "./App.css";
 
 function App() {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const [color, setColor] = useState("grey");
+
+  const getQuote = () => {
+    fetch(
+      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const allQuotes = data.quotes;
+        const randomQuoteNumber = Math.floor(Math.random() * allQuotes.length);
+        const randomQuote = allQuotes[randomQuoteNumber];
+        console.log(randomQuote);
+
+        setQuote(randomQuote.quote);
+        setAuthor(randomQuote.author);
+      })
+      .catch((err) => console.error("Sorry"));
+  };
+
   return (
     <>
       <div className="App">
-        <div className="text">
-          <div className="quote">
+        <div className="quote">
+          <div className="text">
             <FaQuoteLeft />
+            <span className="cit">{quote}</span>
           </div>
-          <div className="author">-</div>
+          <div className="author">- {author}</div>
         </div>
 
         <div className="buttons">
@@ -23,13 +47,16 @@ function App() {
           >
             <FaTwitter />
           </a>
-          <button className="button">New Quote</button>
+
+          <button className="button" onClick={() => getQuote()}>
+            New Quote
+          </button>
         </div>
       </div>
       <footer>
-        by 
+        by
         <a href="https://github.com/Oksana796" target="_blank" rel="noreferrer">
-           Oksana
+          Oksana
         </a>
       </footer>
     </>
